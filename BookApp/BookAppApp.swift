fe40771be97c22f5bd6af663beb68290e35997a4
Tony-Kara <eniolaanthony@gmail.com>
2021-09-25 21:28:22 +0600

    Initial Commit

---
+++ BookApp/BookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by mac on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
